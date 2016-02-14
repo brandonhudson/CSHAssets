38c4f74efa4e11cce1f92bbcc4be0beeeb262278
Bradon Hudson <bhud07@gmail.com>
2016-02-13 20:38:42 -0500

    Angular Templating
    Angular templating

---
+++ frontend/resources/js/app.js
@@ -0,0 +1,74 @@
+var app = angular.module('CSHAssets', []);
+
+app.controller('assetController', function ($scope, $http) {
+    $http.get('/list_files')
+    .success(function(data, status, headers, config) {
+        console.log(data.files); //debug
+        $scope.data = data.files;
+        
+  })
+  .error(function(data, status, headers, config) {   
+        alert("Cannot get assets.") //debug
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+    $scope.setSrc  = function(asset){
+        if(asset.indexOf('.js') > -1){
+            return "resources/images/js.png";
+        }
+        else if(asset.indexOf('.css') > -1){
+            return "resources/images/css.png";
+        }
+        else if(asset.indexOf('.php') > -1){
+            return "resources/images/js.php";
+        }
+        else if(asset.indexOf('.html') > -1){
+            return "resources/images/html.png";
+        }
+        else if(asset.indexOf('.png') > -1 || asset.indexOf('.jpg') > -1 || asset.indexOf('.jpeg') > -1 || asset.indexOf('.gif') > -1 || asset.indexOf('.svg') > -1 || asset.indexOf('.pdf') > -1){
+            return "https://assets.csh.rit.edu/uploads/"+asset;
+        }
+        else{
+            return "resources/images/unknown.png";
+        }
+    }
+    
+    
+
+
+});
+
+app.controller('navController', function ($scope, $http) {
+    $http.get('../resources/api/?submission')
+    .success(function(data, status, headers, config) {
+        console.log(data) //debug
+        $scope.submissions = data.data[0].config_value;
+        
+  })
+  .error(function(data, status, headers, config) {   
+        notify("error","Cannot get submission status.") //debug
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+    
+  $scope.toggleSubmission = function(val){
+     toggleSubmission(val, function(success){
+        if(val == "F"){
+            if(success){
+                $scope.submissions = "F";
+            }
+          
+        }
+         else{
+             if(success){
+                $scope.submissions = "T";
+            }
+          
+        }  
+         
+     });
+      
+  }
+
+
+});
---
+++ frontend/resources/js/assets.js
@@ -0,0 +1,43 @@
+$.material.init()
+$('.btn-copy').tooltip()
+
+$('.btn-copy').click(function(){
+    newValue = "Copied!";
+    oldValue = "Copy to Clipboard";
+   $(this).tooltip('hide')
+          .attr('data-original-title', newValue)
+          .tooltip('fixTitle')
+          .tooltip('show').attr('data-original-title', oldValue)
+          .tooltip('fixTitle'); 
+    
+});
+
+
+var clipboard = new Clipboard('.btn');
+
+clipboard.on('success', function(e) {
+    
+
+    e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+    alert("Error copying asset!");
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+});
+
+
+var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
+var is_explorer = navigator.userAgent.indexOf('MSIE') > -1;
+var is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
+var is_safari = navigator.userAgent.indexOf("Safari") > -1;
+var is_opera = navigator.userAgent.toLowerCase().indexOf("op") > -1;
+if ((is_chrome)&&(is_safari)) {is_safari=false;}
+if ((is_chrome)&&(is_opera)) {is_chrome=false;}
+
+if (is_safari) $('.btn-copy').hide();
+
+
+    
+
